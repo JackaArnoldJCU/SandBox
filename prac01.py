ddadc40943ab7caaa0dd47f3929afb9ad90d2a3e
Jacka Arnold <jackaarnold@outlook.com>
2020-08-14 23:35:11 +1000

    adding prac01

--- prac01.py
+++ prac01.py
@@ -4,14 +4,20 @@ If sales are under $1,000, the user gets a 10% bonus.
 If sales are $1,000 or over, the bonus is 15%.
 """
 sales = float(input("Enter sales: $"))
-if sales >= 1000:
-print(sales * 1.15)
-elif sales <= 1000:
-print (sales * 1.1)
-total_price = 0
-items = int(input("Number of items:"))
-for i in range (items)
-:
-item_price = float(input("Price:"))
-total_price = total_price + item_price
-print("Total is $",total_price)
+
+
+def main():
+
+    if sales >= 1000:
+        print(sales * 1.15)
+    elif sales <= 1000:
+        print(sales * 1.1)
+        total_price = 0
+    items = int(input("Number of items:"))
+    for i in range (items):
+        item_price = float(input("Price:"))
+        total_price = total_price + item_price
+        print("Total is $",total_price)
+
+
+main()
